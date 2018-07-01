7a7434ab380fd624d6985f033f79c9269b684cbc
rachel.koldenhoven <rachel@koldenfrozen.com>
2018-07-01 18:52:18 +0530

    add Spring backend

---
+++ src/main/java/com/koldenfrozen/goals/GoalsApplication.java
@@ -0,0 +1,12 @@
+package com.koldenfrozen.goals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/koldenfrozen/goals/model/Goal.java
@@ -0,0 +1,17 @@
+package com.koldenfrozen.goals.model;
+
+public class Goal {
+    private String name;
+
+    public Goal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
