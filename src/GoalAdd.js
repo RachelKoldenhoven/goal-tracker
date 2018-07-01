a8ad24c18ae4389054cae53a136510c3c2e33ed6
rachel.koldenhoven <rachel@koldenfrozen.com>
2018-07-01 11:26:37 +0530

    type goal, add save btn

--- src/GoalAdd.js
+++ src/GoalAdd.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 
 export default class GoalAdd extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            goal: ''
+        }
+    }
+
+    onChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({...this.state, [name]: value});
+    };
+
     render() {
         return (
             <div>
                 <p>Add a goal</p>
-                <input name="goal"/>
+                <input onChange={this.onChange}
+                       name="goal"
+                value={this.state.goal}/>
+                <button>Save Goal</button>
             </div>
         )
     }
--- src/GoalAdd.test.js
+++ src/GoalAdd.test.js
@@ -6,9 +6,22 @@ import GoalAdd from './GoalAdd';
 describe('GoalAdd', () => {
     it('should render inputs for new goal', () => {
         // Setup
-        const personAddWrapper = shallow(<GoalAdd/>);
+        const goalAddWrapper = shallow(<GoalAdd/>);
 
         // Assert
-        expect(personAddWrapper.find({name: 'goal'})).toHaveLength(1);
+        expect(goalAddWrapper.find({name: 'goal'})).toHaveLength(1);
+        expect(goalAddWrapper.find('button').text()).toEqual('Save Goal');
+    });
+
+    it('should edit the new goal', () => {
+        // Setup
+        const goalAddWrapper = shallow(<GoalAdd/>);
+        const event = {target: {name: 'goal', value: 'I weigh 58 kilos'}};
+
+        // Exercise
+        goalAddWrapper.find({name: 'goal'}).simulate('change', event);
+
+        // Assert
+        expect(goalAddWrapper.find({value: 'I weigh 58 kilos'})).toHaveLength(1);
     });
 });
